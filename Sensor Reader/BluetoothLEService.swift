0b45f0b0dd93cbc461a1130043b33ee19d0605da
Filip Zymek <fzy@spyro-soft.com>
2022-03-11 20:28:38 +0100

    Add BT Scanning

--- Sensor Reader/BluetoothLEService.swift
+++ Sensor Reader/BluetoothLEService.swift
@@ -20,6 +20,11 @@ class BluetoothLEService: NSObject, ObservableObject {
         centralManager = CBCentralManager(delegate: self, queue: nil)
     }
     
+    
+    func startScanning() {
+        centralManager?.scanForPeripherals(withServices: nil, options: nil)
+    }
+    
 }
 
 extension BluetoothLEService: CBCentralManagerDelegate {
@@ -28,10 +33,27 @@ extension BluetoothLEService: CBCentralManagerDelegate {
         self.state = central.state
         
         if central.state == .poweredOn {
-//            startScanning()
-        } else {
-            //do smth else
+            startScanning()
         }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        print("\(peripheral), \(peripheral.services)")
+    }
+}
+
+extension BluetoothLEService: CBPeripheralDelegate {
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        print("Connected to \(peripheral)")
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        print("Finished service discovery with error \(error?.localizedDescription ?? "")")
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverIncludedServicesFor service: CBService, error: Error?) {
+        print("discovered service: \(service)")
+        
     }
     
 }
