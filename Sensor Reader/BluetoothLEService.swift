5dcf8626da0b66d28d61fd781d0c16b498b9de98
Filip Zymek <fzy@spyro-soft.com>
2022-03-18 07:59:23 +0100

    Implement reading service, characteristics and descriptors of iNode sensor

--- Sensor Reader/BluetoothLEService.swift
+++ Sensor Reader/BluetoothLEService.swift
@@ -8,7 +8,6 @@
 import Foundation
 import CoreBluetooth
 import SwiftUI
-import CocoaLumberjack
 import CocoaLumberjackSwift
 
 class BluetoothLEService: NSObject, ObservableObject {
@@ -16,6 +15,20 @@ class BluetoothLEService: NSObject, ObservableObject {
     @Published var state: CBManagerState?
     
     private var centralManager: CBCentralManager?
+    private var discoveredDevices: Set<CBPeripheral> = Set()
+    private var sensor: CBPeripheral!
+    
+    private var numberOfServices = 0
+    private var numberOfCharacteristics = 0
+    private var serviceCharacteristicsMap: [CBService: [CBCharacteristic]] = [:]
+    private var characteristicDescriptorMap: [CBCharacteristic: [CBDescriptor]] = [:]
+    
+    private let queue: OperationQueue = {
+        let q = OperationQueue()
+        q.name = "scanning-queue"
+        q.maxConcurrentOperationCount = 1
+        return q
+    }()
     
     override init() {
         super.init()
@@ -24,6 +37,7 @@ class BluetoothLEService: NSObject, ObservableObject {
     
     
     func startScanning() {
+        DDLogInfo("Starting BT Scan...")
         centralManager?.scanForPeripherals(withServices: nil, options: nil)
     }
     
@@ -40,22 +54,140 @@ extension BluetoothLEService: CBCentralManagerDelegate {
     }
     
     func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
-        DDLogInfo("\(peripheral), \(peripheral.services ?? [])")
+        DDLogInfo("Found \(peripheral.displayName), \(RSSI) dB")
+        discoveredDevices.insert(peripheral)
+        if let name = peripheral.name, name == "iNode-4412D2" {
+            DDLogInfo("Stopping scan. Connecting to \(peripheral.displayName)...")
+            DDLogInfo("Ad data for \(peripheral.displayName) = \(advertisementData)")
+            centralManager?.stopScan()
+            centralManager?.connect(peripheral, options: nil)
+        }
     }
 }
 
 extension BluetoothLEService: CBPeripheralDelegate {
     func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
-        DDLogInfo("Connected to \(peripheral)")
+        DDLogInfo("Connected to \(peripheral.displayName)")
+        sensor = peripheral
+        sensor.delegate = self
+        
+        discoveredDevices.removeAll()
+        
+        DDLogInfo("Starting service discovery for \(sensor.displayName)")
+        sensor.discoverServices(nil)
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
+        DDLogInfo("Disconnected from \(peripheral.displayName)")
+        if let error = error {
+            DDLogInfo("Error: \(error.localizedDescription)")
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: Error?) {
+        DDLogInfo("Failed connection to \(peripheral.displayName) due to \(error?.localizedDescription ?? "Unknown error")")
     }
     
     func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
-        DDLogInfo("Finished service discovery with error \(error?.localizedDescription ?? "")")
+        DDLogInfo("Discovered services for \(peripheral.displayName)")
+        
+        guard let services = peripheral.services else {
+            DDLogInfo("No services found for \(peripheral.displayName), disconnecting...")
+            sensor = nil
+            return
+        }
+        
+        numberOfServices = services.count
+        for s in services {
+            DDLogInfo("Discovering characteristics for service \(s.description) on peripheral: \(peripheral.displayName)")
+            peripheral.discoverCharacteristics(nil, for: s)
+        }
     }
     
-    func peripheral(_ peripheral: CBPeripheral, didDiscoverIncludedServicesFor service: CBService, error: Error?) {
-        DDLogInfo("discovered service: \(service)")
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+        
+        if let error = error {
+            DDLogInfo("Disconnecting due to error when discovering characteristics for service \(service.description). Error: \(error.localizedDescription)")
+            sensor = nil
+        }
+        
+        DDLogInfo("Discovered characteristics for service \(service.description) on \(peripheral.displayName)")
+        guard let characteristics = service.characteristics else {
+            DDLogInfo("No characteristics found for \(service.description), disconnecting...")
+            sensor = nil
+            return
+        }
+        
+        serviceCharacteristicsMap[service] = characteristics
+        numberOfCharacteristics += characteristics.count
+        
+        if serviceCharacteristicsMap.keys.count == numberOfServices {
+            for (service, char) in serviceCharacteristicsMap {
+                for c in char {
+                    peripheral.discoverDescriptors(for: c)
+                }
+            }
+        }
         
     }
     
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverDescriptorsFor characteristic: CBCharacteristic, error: Error?) {
+        if let error = error {
+            DDLogInfo("Error when scanning for descriptors of \(characteristic.uuid). Error: \(error.localizedDescription)")
+            return
+        }
+        
+        guard let descriptors = characteristic.descriptors else {
+            DDLogInfo("No descriptors found found for \(characteristic.uuid)")
+            return
+        }
+        
+        characteristicDescriptorMap[characteristic] = descriptors
+        
+        if characteristicDescriptorMap.keys.count == numberOfCharacteristics {
+            for (s, chars) in serviceCharacteristicsMap {
+                DDLogInfo("Service: \(s.uuid)")
+                for c in chars {
+                    DDLogInfo("Characteristic: \(c.uuid) -> value \(String(describing: c.value))")
+                    for d in characteristicDescriptorMap[c] ?? [] {
+                        DDLogInfo("Descriptor: \(d.uuid)")
+                    }
+                    peripheral.readValue(for: c)
+                }
+            }
+            
+            DDLogInfo("Start reading values of characteristis ...")
+            for c in characteristicDescriptorMap.keys {
+                peripheral.readValue(for: c)
+            }
+            
+            sensor = nil
+        }
+    
+        
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
+        if let error = error {
+            DDLogInfo("Error when reading value of \(characteristic.uuid). Error: \(error.localizedDescription)")
+            return
+        }
+        
+        guard let value = characteristic.value else {
+            DDLogInfo("No value found for \(characteristic.uuid)")
+            return
+        }
+        
+        DDLogInfo("Value of \(characteristic.uuid) = \(String(data: value, encoding: .utf8) ?? "unknown")")
+    }
+        
 }
+
+
+
+extension CBPeripheral {
+    var displayName: String {
+        return name ?? identifier.uuidString
+    }
+}
+
